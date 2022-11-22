c7c2473e82208928b3a56edf2dfa84b00365e8e1
Kelly Christensen <kelly.christensen@sciencespo.fr>
2022-11-22 18:02:57 +0100

    linter and next steps

--- fetch.py
+++ fetch.py
@@ -1,3 +1,4 @@
+"""Main module"""
 import csv
 import os
 
@@ -9,12 +10,12 @@ from enrich import enrich_row
 from exceptions import CSVHeaderError, FileFormatError
 
 
-# Color codes for error messages
-yellow = "\033[1;33m\n"
-reset = "\033[0m"
+#Color codes for error messages
+YELLOW = "\033[1;33m\n"
+RESET = "\033[0m"
 
 
-class Arguments(object):
+class Arguments():
     """Verifies that all the CLI arguments are valid and assigns values to key parameters
         necessary for functions later in the workflow.
 
@@ -24,42 +25,46 @@ class Arguments(object):
         urls (str): value of --urls [required], name of the URLs column
         domains (str): value of --domains [optional], name of the domains column
         n (bool): value of --n [optional], declaration of whether the URLs are normalized
-    
+
     Returns:
-        Arguments().archive_path (str): path to the archive *important*
-        Arguments().infile_path (str): path to the data file *important*
-        Arguments().url_col (str): name of the URLs column *important*
-        Arguments().normalized (bool): whether the URLs are normalized
-        Arguments().normalized_url_col (str): either the name of the column in the data file contains normalized URLs 
-            or the name of the column in the out-file that will contain normalized URLs *important*
-        Arguments().infile_fieldnames (list): list of column headers in data file
-        Arguments().enriched_fieldnames (list): list of column headers for out-file *important*
+    Arguments().archive_path (str): path to the archive *important*
+    Arguments().infile_path (str): path to the data file *important*
+    Arguments().url_col (str): name of the URLs column *important*
+    Arguments().normalized (bool): whether the URLs are normalized
+    Arguments().normalized_url_col (str): either the name of the column in
+        the data file contains normalized URLs or the name of the column in
+        the out-file that will contain normalized URLs *important*
+    Arguments().infile_fieldnames (list): list of column headers in
+        data file
+    Arguments().enriched_fieldnames (list): list of column headers for
+        out-file *important*
     """
-    def __init__(self, archive, infile, urls, domains, n):
+    def __init__(self, archive, infile, urls, domains, normalized):
         self.archive_path = archive
         self.infile_path = infile
         self.url_col = urls
         self.domain_col = domains
-        self.normalized = n
         self.normalized_url_col = None
         self.infile_fieldnames = []
         self.enriched_fieldnames = ["archive_subdirectory", "archive_timestamp"]
 
         # Verify that the archive is a directory
         if not os.path.isdir(self.archive_path):
-            raise NotADirectoryError(f"{yellow}{self.archive_path} is not a directory.{reset}")
+            raise NotADirectoryError(f"{YELLOW}{self.archive_path} is not a directory.{RESET}")
 
         # Verify that the infile is a file
         if not os.path.isfile(self.infile_path):
-            raise FileNotFoundError(f"{yellow}{self.infile_path} is not a file.{reset}")
+            raise FileNotFoundError(f"{YELLOW}{self.infile_path} is not a file.{RESET}")
 
-        with open(self.infile_path, "r") as f:
+        with open(self.infile_path, "r", encoding="utf-8") as csvfile:
             # Verify that the CSV can be opened with headers
             try:
-                reader = csv.DictReader(f)
+                reader = csv.DictReader(csvfile)
                 self.infile_fieldnames.extend(reader.fieldnames)
-            except:
-                raise FileFormatError(f"{yellow}Unable to read {self.infile_path} as CSV file.{reset}")
+            except Exception as exception:
+                raise FileFormatError(f"\
+                {YELLOW}Unable to read {self.infile_path} as CSV file.{RESET}")\
+                from exception
 
             # Set up a row of data to test
             test_row = next(reader)
@@ -67,9 +72,10 @@ class Arguments(object):
             # Verify that the given name of the URLs column is in the CSV
             if not test_row.get(self.url_col):
                 raise CSVHeaderError(column=self.url_col)
-            
+
             # If declared to be normalized, verify that the URLs column contains normalized URLs
-            if not self.normalized or test_row[self.url_col] != ural_normalize_url(test_row[self.url_col]):
+            if not normalized or test_row[self.url_col] \
+                != ural_normalize_url(test_row[self.url_col]):
                 self.enriched_fieldnames.append("normalized_url")
                 self.normalized_url_col = "normalized_url"
             else:
@@ -82,28 +88,34 @@ class Arguments(object):
             if not self.domain_col:
                 self.enriched_fieldnames.append("domain")
                 self.domain_col = "domain"
-            
+
             self.enriched_fieldnames.extend(self.infile_fieldnames)
 
 
-def generate_reader(fp):
+def generate_reader(infile_path):
     """Yields rows from the CSV reader."""
-    with open(fp, "r") as csvfile:
+    with open(infile_path, "r", encoding="utf-8") as csvfile:
         yield from csv.DictReader(csvfile)
 
 
 @click.command()
-@click.option("--archive", type=click.Path(exists=True), required=True, help="Path to archive in which subdirectories of archived HTML will be stored.")
-@click.option("--infile", type=click.Path(exists=True), required=True, help="Path to the CSV file containing the URLs to be processed.")
-@click.option("--urls", type=str, required=True, help="Header of column containing URLs.")
-@click.option("--domains", type=str, required=False, help="Header of column containing domain names.")
-@click.option("-n", type=bool, is_flag=True, default=False, required=False, help="Flag indicating the URLs are already normalized.")
-@click.option("--outfile", "outfile_path", type=str, default="outfile.csv", required=False, help="Name of or path to the file in which the enriched data will be output.")
-def cli(archive, infile, urls, domains, n, outfile_path):
-    args = Arguments(archive, infile, urls, domains, n)
-
-    with open(outfile_path, "w") as f:
-        writer = csv.DictWriter(f, fieldnames=args.enriched_fieldnames)
+@click.option("--archive", type=click.Path(exists=True), required=True, \
+    help="Path to archive in which subdirectories of archived HTML will be stored.")
+@click.option("--infile", type=click.Path(exists=True), required=True, \
+    help="Path to the CSV file containing the URLs to be processed.")
+@click.option("--urls", type=str, required=True, \
+    help="Header of column containing URLs.")
+@click.option("--domains", type=str, required=False, \
+    help="Header of column containing domain names.")
+@click.option("-n", "normalized", type=bool, is_flag=True, default=False, required=False, \
+    help="Flag indicating the URLs are already normalized.")
+@click.option("--outfile", "outfile_path", type=str, default="outfile.csv", required=False, \
+    help="Name of or path to the file in which the enriched data will be output.")
+def cli(archive, infile, urls, domains, normalized, outfile_path):
+    """Parses CLI arguments, parses the CSV file, and outputs a new CSV."""
+    args = Arguments(archive, infile, urls, domains, normalized)
+    with open(outfile_path, "w", encoding="utf-8") as csvwriter:
+        writer = csv.DictWriter(csvwriter, fieldnames=args.enriched_fieldnames)
         writer.writeheader()
         for row in generate_reader(args.infile_path):
             row = enrich_row(args, row)
---
+++ nextSteps.md
@@ -0,0 +1,106 @@
+# Next Steps
+
+## Getting Started
+
+1. Cone this Git repository.
+2. Create a virtual environment and activate it.
+3. Install the dependencies: `click`, `minet`, `pylint`, and `ural`
+```bash
+$ pip install click minet pylint ural
+```
+
+## Step 1.
+
+Read the [README.md](README.md) file and existing Python modules to familiarize yourself with the respository.
+
+1. Understand the program's objective.
+
+    - What sort of data does the program take in? What does the incoming data look like / how is it structured?
+
+    - What does the program yield?
+
+2. Understand the program's architecture / approach to the objective.
+
+    - How does [fetch.py](fetch.py) parse arguments from the Command Line Interface (CLI)?
+
+    - How does [fetch.py](fetch.py) parse the CSV file?
+
+    - How does [fetch.py](fetch.py) write the out-file?
+
+    - What data (arguments) do the modules [enrich.py](enrich.py) and [archive.py](archive.py) take? What does each one need to return?
+
+3. Try it out.
+
+While enrich.py and fetch.py simply return the row as is, you can test the incomplete program on the data in `test_data.csv`.
+```bash
+$ mkdir archive
+$ python fetch.py --archive archive/ --infile test_data.csv --urls source_url
+```
+Without the option `--outfile`, the result should appear at the outfile's default file path. You can find what the default is by examining the `--outfile` Click option, which decorates the `cli()` command in [fetch.py](fetch.py). The Click option has a parameter `default=`.
+
+## Step 2.
+
+Create a new branch on which to develop the new `enrich_row()` function. Give it a meanintful title, like `dev_enrich`. You will develop the function on this development branch. Eventually, once the function is ready (Step 3 is done), you'll "merge" the `dev_enrich` branch to the main branch and push your merged main branch to the remote.
+
+```mermaid
+    gitGraph
+       commit
+       commit
+       branch dev_enrich
+       commit
+       commit
+       commit
+       checkout main
+       merge dev_enrich
+```
+>Note: The first time you push commits on a new local branch to the remote (origin) repository, you will need to tell the remote that it needs to make a new branch and you'll need to help it name the new remote branch.
+
+## Step 3.
+
+Complete the function `enrich_row()` in the module [enrich.py](enrich.py) so that it returns a dictionary `row` object with values in the keys `normalized_url_col` and `domain`. Do not overwrite these values if the row's dictionary already has them (aka, if the data file already had the normalization and domain name for the URL in that row).
+
+Make use of URAL's functions `normalize_url()` and `get_domain()`.
+
+As you develop `enrich_row()` (and any helper functions in the module), "lint" your code to make sure it's clean and conforms to Python's norms. To "lint" your code in `enrich.py`, type the following command in the terminal:
+```bash
+$ pylint --rcfile .pylint enrich.py
+```
+Try to clean up any issues the linter finds. Note: Sometimes it's ok to break the norms and not follow the linter's advice, but it's good to have a good reason.
+
+## Step 4.
+
+Make another branch! Each branch should serve one specific purpose, so make a new branch for the development of `archive_row()`. You can name it something like `dev_archive`.
+
+```mermaid
+    gitGraph
+       commit
+       commit
+       branch dev_enrich
+       commit
+       commit
+       commit
+       checkout main
+       merge dev_enrich
+       branch dev_archive
+       commit
+       commit
+       commit
+       commit
+       commit
+       checkout main
+       merge dev_archive
+```
+
+## Step 5.
+
+Complete the function `archive_row()` in [archive.py](archive.py) so that it returns a dictionary `row` object with values in the keys `archive_subdirectory` and `archive_timestamp`. Do not give values to these keys if the normalized URL's HTML was not able to be archived.
+
+Make use of Minet's functions.
+
+> Hint: We want to call the normalized versions of the URLs in the dataset. Normalized URLs lack what's called a "protocol", in example "https://". However, Minet cannot fetch a URL that lacks a protocol. You'll need to respond to this.
+
+And, again, use the command `pylint` in the terminal to check your code.
+
+```bash
+$ pylint --rcfile .pylintrc archive.py
+```
